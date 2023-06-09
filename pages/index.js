9ee7d625680ac38ff16d3da75f1e61faa9079e44
kakalota <shadow_dragon@126.com>
2023-06-09 10:36:20 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from 'react';
 import { useSession, signIn, signOut } from "next-auth/react";
-
-
 
 export default function Home() {
   const { data: session } = useSession();
